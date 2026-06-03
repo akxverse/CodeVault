abe3ea0d85b5900063aac8ef2007f909fef5e72b
akxverse <aakankshaasingh18@gmail.com>
2026-06-03 23:54:20 +0530

    Added diamond pattern program

---
+++ C/diamond_pattern.c
@@ -0,0 +1,24 @@
+//Print a diamond....
+#include <stdio.h>
+
+int main() {
+int n, i, j;
+printf("Enter half-size of diamond: ");
+scanf("%d", &n);
+
+// upper half (including middle).
+for (i = 1; i <= n; i++) {
+for (j = i; j < n; j++) printf(" "); // spaces
+for (j = 1; j <= (2*i-1); j++) printf("*"); // stars
+printf("\n");
+}
+
+// lower half.
+for (i = n-1; i >= 1; i--) {
+for (j = n; j > i; j--) printf(" "); // spaces
+for (j = 1; j <= (2*i-1); j++) printf("*"); // stars
+printf("\n");
+}
+
+return 0;
+}
