78dddfddeda23441bbba74d9271a898aa55fa707
akxverse <aakankshaasingh18@gmail.com>
2026-06-04 23:37:57 +0530

    Added vowel and consonant counter

---
+++ C/Strings/count_vowels.c
@@ -0,0 +1,30 @@
+// Count Vowels and Consonants
+#include <stdio.h>
+#include <ctype.h> // for tolower()
+
+int main() {
+char str[100];
+int i, vowels = 0, consonants = 0;
+
+printf("Enter a string: ");
+fgets(str, sizeof(str), stdin);//Takes a full line as input.
+
+for(i = 0; str[i] != '\0'; i++) {
+
+    char ch = tolower(str[i]);
+
+    if(ch >= 'a' && ch <= 'z') {
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' ||ch == 'o' || ch == 'u')
+            vowels++;
+        else
+            consonants++;
+    }
+}
+
+printf("Vowels = %d\n", vowels);
+
+printf("Consonants = %d\n", consonants);
+
+return 0;
+}
