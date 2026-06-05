aa9c8f021f4123bb10cc7057f140101032949e99
akxverse <aakankshaasingh18@gmail.com>
2026-06-05 23:30:51 +0530

    Added array sum and average program

---
+++ C/Arrays/array_sum_avg.c
@@ -0,0 +1,25 @@
+// Calculate the sum and average of all elements in an array.
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0;
+
+    printf("How many elements? ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum = %.1f\n", sum);
+
+    printf("Average = %.2f\n", sum / n);
+
+    return 0;
+}
