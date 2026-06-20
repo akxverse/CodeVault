21192cedf72b25268b581bac6693efbd77410173
akxverse <aakankshaasingh18@gmail.com>
2026-06-20 23:30:35 +0530

    Added to-do list project

---
+++ Python/Mini Projects/to_do_list.py
@@ -0,0 +1,51 @@
+# To-Do List App
+
+tasks = []
+def show_tasks():
+    if not tasks:
+        print("No tasks yet!")
+        return
+    
+    print("\nYour Tasks:")
+    print("-" * 30)
+
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task}")
+
+    print("-" * 30)
+
+
+while True:
+
+    print("\n===== TO-DO LIST =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == "2":
+        show_tasks()
+
+    elif choice == "3":
+        show_tasks()
+
+        if tasks:
+            num = int(input("Enter task number to delete: "))
+            if 1 <= num <= len(tasks):
+                removed = tasks.pop(num - 1)
+                print(f"Deleted: {removed}")
+            else:
+                print("Invalid task number!")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice! Please try again.")
